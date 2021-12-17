0cdd6f45a670e8a1fbf4628fcc41ba883190a4c9
josedavidp <jdpiedra1@utpl.edu.ec>
2021-12-17 11:52:52 -0500

    mensaje

--- EjerciciosT9/src/paquete1/Ejercicio03.java
+++ EjerciciosT9/src/paquete1/Ejercicio03.java
@@ -16,10 +16,29 @@ public class Ejercicio03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        int elementosArribaMedia;
-        int elementosAbajoMedia;
-        
+        double[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
+        int elementosArribaMedia = 0;
+        int elementosAbajoMedia = 0;
+        double media = 0;
+        double suma = 0;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+        }
+        media = suma / arreglo.length;
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] < media) {
+                elementosAbajoMedia = elementosAbajoMedia + 1;
+            } else {
+                if (arreglo[i] < media) {
+                    elementosArribaMedia = elementosArribaMedia + 1;
+                }
+            }
+
+        }
+        System.out.printf("Media: %.2f\nElementos arriba de la media: %d\n"
+                + "Elementos abajo de la media: %d\n", media, elementosArribaMedia,
+                 elementosAbajoMedia);
     }
 
 }
